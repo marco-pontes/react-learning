10e2c0bc4f01462a91a7424c93d120d0b84d4e72
marco-pontes <marco.pontes@resultadosdigitais.com>
2018-09-04 23:49:23 -0300

    Add React-router

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,14 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-grid.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+    (<BrowserRouter >
+        <div>
+            <Route path="/" component={App} />
+            <Route path="/autor" />
+            <Route path="/livro" />
+        </div>
+    </BrowserRouter>),
+    document.getElementById('root'));
